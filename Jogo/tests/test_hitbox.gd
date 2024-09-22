48d83c4f8566c26e35e2075fec41450496143b17
Bassani-Paulo <bassanipaulo@gmail.com>
2024-09-22 18:24:04 -0300

    unit test

---
+++ Jogo/tests/test_hitbox.gd
@@ -0,0 +1,30 @@
+extends GutTest
+
+var player
+var lixo
+var double_jogo
+
+# Create a mock player that will be used in the test
+func before_each():
+	double_jogo = double(load("res://scenes/Tartaruga/Jogo.tscn")).instantiate()
+	player = load("res://scenes/Tartaruga/Player.tscn").instantiate()
+	lixo = load("res://scenes/Tartaruga/Lixo.tscn").instantiate()
+	
+	add_child(double_jogo)
+	double_jogo.add_child(player)
+	double_jogo.add_child(lixo)
+
+func test_player_takes_damage_on_collision():
+	player.get_node("Area2D").emit_signal("area_entered", lixo.get_node("Area2D"))
+	await wait_frames(2)
+	assert_called(double_jogo, "take_damage")
+
+func test_lixo_is_removed_after_collision():
+	player.get_node("Area2D").emit_signal("area_entered", lixo.get_node("Area2D"))
+	await wait_frames(2)
+	assert_false(is_instance_valid(lixo))
+
+#func after_all():
+	#double_jogo.queue_free()
+	#player.queue_free()
+	#lixo.queue_free()
