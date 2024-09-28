4090cac4c8c349c34d5f239f6fca6b28afadbd6c
Bassani-Paulo <32801209+Bassani-Paulo@users.noreply.github.com>
2024-09-27 22:12:08 -0300

    Revert "Commit contendo movimentação"

--- Jogo/scripts/Player.gd
+++ Jogo/scripts/Player.gd
@@ -1,25 +1,11 @@
-extends Area2D
+extends Node2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
-	velocity.x = 0            ## Constant player movement
-	
-	##Player Movement change
-	if Input.is_action_pressed("move_right"): 
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if velocity.length() > 0 :
-		velocity = velocity.normalized() * speed
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
-	
+func _process(delta):
 	pass
