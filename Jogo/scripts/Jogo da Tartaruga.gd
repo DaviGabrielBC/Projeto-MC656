7a976ba1a22578e8291fce0cacee06792d9d9e84
gustavoamaraal <g218669@dac.unicamp.br>
2024-10-01 21:28:17 -0300

    mudanças pedidas no pull request feitas

--- Jogo/scripts/Jogo da Tartaruga.gd
+++ Jogo/scripts/Jogo da Tartaruga.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
-var max_hp: int
+const max_hp: int = 3
 var player_hp: int
-var player_alive: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	max_hp = 100
-	player_hp = 100
-	player_alive = true
+	player_hp = 3
 	update_hp_label()
 	pass
 
 # Function to handle taking damage
-func take_damage(damage):
+func take_damage(damage = 1):
 	player_hp -= damage
 	if player_hp <= 0:
 		player_hp = 0
@@ -22,12 +19,11 @@ func take_damage(damage):
 
 # Function to handle the game over condition
 func game_over():
-	player_alive = false
 	pass
 
 # Utility function to update the HP label (use signal or direct reference to HUD)
 func update_hp_label():
-	$Hud/hp_label.text = "HP: " + str(player_hp) + "/" + str(max_hp)
+	$Hp_label.text = "HP: " + str(player_hp) + "/" + str(max_hp)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Jogo/tests/test_hp.gd
+++ Jogo/tests/test_hp.gd
@@ -1,55 +1,55 @@
 extends GutTest
 
-var PlayerScene = preload("res://scenes/Tartaruga/Jogo.tscn") 
-var player 
+var JogoScene = preload("res://scenes/Tartaruga/Jogo.tscn") 
+var jogo
+var double_jogo
 
 func before_each():
-	# Create a new instance of the Player scene before each test
-	player = PlayerScene.instantiate()
+	# Create new instances of the Jogo scene before each test
+	jogo = JogoScene.instantiate()
+	double_jogo = partial_double(JogoScene).instantiate()
 
 # Test if the player takes damage correctly
 func test_player_takes_damage():
 	# Set initial health
-	player.player_hp = 100
+	jogo.player_hp = 3
 	
-	# Call the take_damage function with 10 damage
-	player.take_damage(10)
+	# Call the take_damage function with 2 damage
+	jogo.take_damage(2)
 	
-	# Assert that health decreased by 10
-	assert_eq(player.player_hp, 90, "Player health should be 90.")
+	# Assert that health decreased by 2
+	assert_eq(jogo.player_hp, 1, "Player health should be 1.")
 	
 # Test if the player hp doesn't gets below zero when they die
 func test_hp_not_below_zero():
 	# Set initial health
-	player.player_hp = 100
+	jogo.player_hp = 3
 	
-	# Call the take_damage function with 110 damage
-	player.take_damage(110)
+	# Call the take_damage function with 8 damage
+	jogo.take_damage(8)
 	
 	# Assert that health reseted to 0
-	assert_eq(player.player_hp, 0, "Player health should be 0.")
+	assert_eq(jogo.player_hp, 0, "Player health should be 0.")
 
 # Test if the player dies when takes lethal damage
 func test_player_dies():
-	# Set health and player_alive
-	player.player_hp = 10
-	player.player_alive = true
+	# Set health 
+	double_jogo.player_hp = 3
 	
 	# Call the take_damage function with lethal damage
-	player.take_damage(50)
+	double_jogo.take_damage(5)
 	
 	# Assert that the player dies
-	assert_eq(player.player_alive, false, "Player shouldn't be alive.")
+	assert_called(double_jogo, "game_over")
 	
 # Test if the HP label shows health correctly
 func test_HP_label():
-	# Set hp and max_hp
-	player.player_hp = 100
-	player.max_hp = 100
-	player.get_node("Hud/hp_label").text = "HP: 100/100"
+	# Set player hp 
+	jogo.player_hp = 3
+	jogo.get_node("Hp_label").text = "HP: 3/3"
 	
-	# Call the take_damage function
-	player.take_damage(50)
+	# Call the take_damage function with 1 damage
+	jogo.take_damage()
 	
 	# Assert that the player HP is showed correctly
-	assert_eq(player.get_node("Hud/hp_label").text, "HP: 50/100", "Player HP should be 50/100")
+	assert_eq(jogo.get_node("Hp_label").text, "HP: 2/3", "Player HP should be 2/3")
