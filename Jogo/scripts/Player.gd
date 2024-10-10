a7d5d0c9148a1f3ddc5d1233ff2a9dc6006aee01
DaviGabrielBC <d183710@dac.unicamp.br>
2024-10-10 15:09:44 -0300

    Atualizando feature/#4

--- Jogo/scripts/Player.gd
+++ Jogo/scripts/Player.gd
@@ -1,17 +1,30 @@
 extends Area2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
+<<<<<<< Updated upstream
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.x = get_viewport().get_visible_rect().size.x / 2
 	pass # Replace with function body.
+=======
+static var test_pressed_right
+static var test_pressed_left
+
+# Called when the player enters the scene for the first time.
+func _ready() -> void:
+	var tartaruga_height = get_node("Sprite2D").texture.get_height()
+	position.x = get_viewport().get_visible_rect().size.x / 2
+	position.y = get_viewport().get_visible_rect().size.y - tartaruga_height / 3
+	pass
+>>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
 	
 	##Player Movement change
+<<<<<<< Updated upstream
 	if Input.is_action_pressed("move_right"): 
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -25,5 +38,28 @@ func _process(delta: float) -> void:
 	position.y = clamp(position.y, visible_rect.position.y, visible_rect.position.y + visible_rect.size.y)
 	#position.x = clamp(position.x, 0, screen_size.x)
 	#position.y = clamp(position.y, 0, screen_size.y)
+=======
+	if Input.is_action_pressed("move_right") or test_pressed_right: 
+		velocity.x += 1
+	if Input.is_action_pressed("move_left") or test_pressed_left:
+		velocity.x -= 1
+	
+	##Vertical Movement won't be used yet
+	#if Input.is_action_pressed("move_up"): 
+	#	velocity.y -= 1
+	#if Input.is_action_pressed("move_down"):
+	#	velocity.y += 1
+	
+	if velocity.length() > 0 :
+		velocity *= speed
+
+	var tartaruga_height = get_node("Sprite2D").texture.get_height()
+	var tartaruga_width = get_node("Sprite2D").texture.get_width()
+	var visible_rect = get_viewport().get_visible_rect()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, tartaruga_width/2,visible_rect.size.x-tartaruga_width/2)
+	position.y = clamp(position.y, tartaruga_height/3, visible_rect.size.y-tartaruga_height/3)
+>>>>>>> Stashed changes
 	
 	pass
--- Jogo/tests/unit/test_movement.gd
+++ Jogo/tests/unit/test_movement.gd
@@ -1,14 +1,30 @@
 extends GutTest
 
+<<<<<<< Updated upstream
 func _ready():
 	var double_jogo = double(load("res://scenes/Tartaruga/Jogo.tscn")).instantiate()
 	var player = load("res://scenes/Tartaruga/Player.tscn").instantiate()
 	var lixo = load("res://scenes/Tartaruga/Lixo.tscn").instantiate()
+=======
+var Player_Script = load("res://scripts/Player.gd").new()
+var elapsed_time = 2.88
+
+var width
+var player
+var lixo
+var double_jogo
+
+func before_each():
+	double_jogo = double(load("res://scenes/Tartaruga/Jogo.tscn")).instantiate()
+	player = load("res://scenes/Tartaruga/Player.tscn").instantiate()
+	lixo = load("res://scenes/Tartaruga/Lixo.tscn").instantiate()
+>>>>>>> Stashed changes
 	
 	add_child(double_jogo)
 	double_jogo.add_child(player)
 	double_jogo.add_child(lixo)
 	
+<<<<<<< Updated upstream
 	# Roda os testes
 	test_player_movement()
 
@@ -35,3 +51,38 @@ func test_player_movement():
 
 	# Clean up - reset player position for potential future tests
 	player.position.x = get_viewport().get_visible_rect().size.x / 2
+=======
+	width = get_viewport().get_visible_rect().size.x
+
+func test_moves_left():
+	# Simulate pressing the left arrow key
+	Player_Script.test_pressed_left = 1
+	player._process(elapsed_time) 
+	Player_Script.test_pressed_left = 0
+
+	assert_lt(int(player.position.x), int(width/2), "Player should have moved left.")
+	
+func test_moves_right():
+	# Simulate pressing the right arrow key
+	Player_Script.test_pressed_right = 1
+	player._process(elapsed_time) 
+	Player_Script.test_pressed_right = 0
+
+	assert_gt(int(player.position.x), int(width/2), "Player should have moved right.")
+
+func test_out_of_bounds_left():
+	# Simulate pressing the left arrow key
+	Player_Script.test_pressed_left = 1
+	player._process(2 * elapsed_time) #Enough time for the player to supposedly exceed the bounds of the visible rectangle
+	Player_Script.test_pressed_left = 0
+
+	assert_lt(int(player.position.x), int(width), "Player is out of bounds on the left side.")
+	
+func test_out_of_bounds_right():
+	# Simulate pressing the right arrow key
+	Player_Script.test_pressed_right = 1
+	player._process(2 * elapsed_time) #Enough time for the player to supposedly exceed the bounds of the visible rectangle
+	Player_Script.test_pressed_right = 0
+
+	assert_gt(int(player.position.x), int(0), "Player is out of bounds on the right side.")
+>>>>>>> Stashed changes
